84de84898697c3a5998412abed657473e7847ea4
ccifuentesc <cavaliercifuentes@gmail.com>
2022-07-24 00:31:32 -0500

    Cambios

--- sobre-mi.php
+++ sobre-mi.php
@@ -246,10 +246,15 @@
     <section id="formacion">
       <div class="container">
         <div class="row">
-          <div class="col 12">
+          <div class="col-12">
             <h2>
               Formación Académica
             </h2>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-2 d-no">
+
           </div>
         </div>
       </div>
@@ -258,7 +263,7 @@
     <section id="cursos">
       <div class="container">
         <div class="row">
-          <div class="col 12">
+          <div class="col-12">
             <h2>
               Cursos
             </h2>
